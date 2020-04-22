5cabd059e9abb427e8873476e4e8c03e300e7a1b
Vatsal Mistry <mistryvatsal11@gmail.com>
2020-04-23 05:28:59 +0530

    Added components to empty files

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const contact = () => {
-  return <div></div>
+  return <div>Hello From contact page.</div>
 }
 
 export default contact
---
+++ src/pages/projects.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const projects = () => {
+  return <div>Hello From projects page.</div>
+}
+
+export default projects
